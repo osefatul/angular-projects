853aac5a105c97e9223321bc1c25c3a481fd3a70
Sefatullah Omar <sefaullahomar1@gmail.com>
2023-03-01 01:26:02 -0800

    creditCard Done

--- 8-creditCardPayments/src/app/app.module.ts
+++ 8-creditCardPayments/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
 import { InputComponent } from './input/input.component';
-
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardFormComponent,
-    InputComponent
+    InputComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
