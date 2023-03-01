37f90d223f0cab571da88991ac480a84902ef6ac
Sefatullah Omar <sefaullahomar1@gmail.com>
2023-02-28 19:57:19 -0800

    creditcard

---
+++ 8-creditCardPayments/src/app/input/input.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss']
+})
+
+export class InputComponent {
+
+
+  @Input() control: any;
+  @Input() label: any;
+
+  constructor(){}
+
+  ngOnInit(): void {}
+  
+  showErrors() {
+    const { dirty, touched, errors } = this.control;
+    return dirty && touched && errors;
+  }
+}
