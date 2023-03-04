3823eaf78493e6584ffad43687a20c4374fcfa9e
Sefatullah Omar <sefaullahomar1@gmail.com>
2023-03-04 14:16:47 -0800

    inbox

--- 11-emailClient/src/app/auth/auth.guard.ts
+++ 11-emailClient/src/app/auth/auth.guard.ts
@@ -13,22 +13,21 @@ import { Observable } from "rxjs";
 import { take, skipWhile, tap } from "rxjs/operators";
 import { AuthService } from "./auth.service";
 
+
+
 @Injectable({
   providedIn: "root"
 })
 export class AuthGuard implements CanLoad {
   constructor(private authService: AuthService, private router: Router) {}
 
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]
-  ): Observable<boolean> | Promise<boolean> | boolean {
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
     return this.authService.signedin$.pipe(
-      skipWhile(value => value === null),
-      take(1),
+      skipWhile(value => value === null),// skip value if is null.
+      take(1),//make it complete after one value
       tap(authenticated => {
         if (!authenticated) {
-          this.router.navigateByUrl("/signup");
+          this.router.navigateByUrl("/");
         }
       })
     );
